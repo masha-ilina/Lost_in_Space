d8526e4478fb8b225bd780ae3973204b4efb6b19
Jake Trujillo <jtrujillo1960@gmail.com>
2017-05-13 14:31:50 -0500

    Added files

---
+++ Item.java
@@ -0,0 +1,12 @@
+package byui.cit260.lost_in_space.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Jake
+ */
+public class Item implements Serializable {
+    
+}
