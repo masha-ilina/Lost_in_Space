39012ae1acfd66bf59d29640ebe872d00235471d
Jake Trujillo <jtrujillo1960@gmail.com>
2017-05-13 14:49:08 -0500

    roughed out basic structure

--- Actor.java
+++ Actor.java
@@ -8,5 +8,72 @@ import java.util.Objects;
  * @author Jake
  */
 public class Actor implements Serializable {
-    
+
+    private String name;
+    private String description;
+    private String status;
+    private int health;
+    private int[] coordinates;
+
+    public Actor() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" + "name=" + name + ", description="+ description + ""+""+'}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+}
 }
